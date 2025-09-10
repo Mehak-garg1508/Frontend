d72c586f58d74bde0e7db73efb30d29117af597f
Mehak Garg <missgunnu1@gmail.com>
2025-09-10 20:43:38 +0530

    Primitive Datatype

---
+++ 03JS/Day2/part1.js
@@ -0,0 +1,32 @@
+// Datatype
+
+// Number
+let num = 24;
+console.log(num);
+console.log(typeof num);
+num = 98.56;
+console.log(num);
+console.log(typeof num);
+
+// String
+let str = "Hello World";
+console.log(str);
+console.log(typeof str);
+
+// Boolean
+let isAdult = false;
+console.log(isAdult);
+console.log(typeof isAdult);
+isAdult = true;
+console.log(isAdult);
+
+// NULL
+// Jan puch kar value na dena
+let n = null;
+console.log(n);
+console.log(typeof n);
+
+// Undefined
+let age;
+console.log(age);
+console.log(typeof age);
---
+++ 03JS/Day2/readme.md
@@ -0,0 +1,9 @@
+Datatype
+
+1. Primitive
+   a) Number
+   b) String
+   c) Boolean
+   d) NULL
+   e) Undefined
+   f) Symbol
