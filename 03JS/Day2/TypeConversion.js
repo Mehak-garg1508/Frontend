e4a962d86d3773d80928ecdd1f4afc9c2e0bc646
Mehak Garg <missgunnu1@gmail.com>
2025-09-10 21:04:40 +0530

    Type Conversion

---
+++ 03JS/Day2/TypeConversion.js
@@ -0,0 +1,48 @@
+// String to number
+let account_balance = "1200";
+console.log(account_balance);
+console.log(typeof account_balance);
+let num = Number(account_balance);
+console.log(num);
+console.log(typeof num);
+let ans = "100ans";
+console.log(Number(ans));
+
+// Boolean to Number
+let x = true;
+console.log(x);
+console.log(typeof x);
+let n = Number(x);
+console.log(n);
+console.log(typeof n);
+console.log(Number(false));
+
+// null
+let x1 = null;
+console.log(typeof x1);
+console.log(Number(x1));
+
+// Undefined
+let x2 = undefined;
+console.log(typeof x2);
+console.log(Number(x2));
+
+// String ke andar convert
+let ab = 20;
+console.log(typeof ab);
+ab = String(ab);
+console.log(ab);
+console.log(typeof ab);
+
+let ax = true;
+console.log(typeof ax);
+console.log(String(ax));
+console.log(typeof String(ax));
+
+// Convert to booleam
+let s1 = "";
+console.log(Boolean(s1));
+let s2 = "abs";
+console.log(Boolean(s2));
+let s3 = " ";
+console.log(Boolean(s3));
