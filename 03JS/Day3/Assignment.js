57043156d51a80b717571a76afbcfb21799fcd97
Mehak Garg <missgunnu1@gmail.com>
2025-09-10 21:26:21 +0530

    Assignment Operator

---
+++ 03JS/Day3/Assignment.js
@@ -0,0 +1,30 @@
+// Assignment operator
+// 20 value is assigned to number num
+let x = 20;
+console.log(x); //20
+
+// addition
+x = x + 10;
+console.log(x); //30
+x += 10;
+console.log(x); //40
+
+// Subtraction
+// x = x - 10
+x -= 10;
+console.log(x); //30
+
+// Multiplication
+// x = x * 10;
+x *= 10;
+console.log(x); //300
+
+// Division
+// x = x / 10;
+x /= 10;
+console.log(x); //30
+
+// Modulus
+// x = x % 10;
+x %= 10;
+console.log(x); //0
