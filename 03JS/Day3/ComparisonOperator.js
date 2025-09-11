c61f805787784627bca86e5632a7d1f86523f5f1
Mehak Garg <missgunnu1@gmail.com>
2025-09-11 22:58:01 +0530

    Comparison Operator

---
+++ 03JS/Day3/ComparisonOperator.js
@@ -0,0 +1,33 @@
+// Comparison Operator
+let a1 = 1;
+let a2 = 2;
+console.log(a1 == a2);
+console.log(a1 > a2);
+console.log(a1 < a2);
+console.log(a1 <= a2);
+console.log(a1 >= a2);
+
+let s = "2";
+// type conversion hua ha string ka number ma
+console.log(a2 == s);
+console.log(a2 < s);
+console.log(a2 > s);
+console.log(a2 >= s);
+console.log(a2 <= s);
+
+// NUll
+console.log(null == undefined);
+// null == undefined true hota ha usko chod kar sab false hote ha
+console.log(null == 0);
+console.log(null < 0);
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null <= 0);
+// type conversion hoga null ka number ma jo ki 0 hota ha
+
+// Undefined
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+console.log(undefined <= 0);
+console.log(undefined >= 0);
