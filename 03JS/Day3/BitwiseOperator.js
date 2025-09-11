37e62dbec348cb9fdb59258d0dbf03acf30bf7bc
Mehak Garg <missgunnu1@gmail.com>
2025-09-11 23:10:48 +0530

    Bitwise and Shift Operator

---
+++ 03JS/Day3/BitwiseOperator.js
@@ -0,0 +1,34 @@
+// Bitwise Operator
+// And (&)
+console.log(4 & 5);
+// 1 0 0
+// 1 0 1
+// 1 0 0
+// Therefore, 4 is answer
+
+// Or (|)
+console.log(4 | 5);
+// 1 0 0
+// 1 0 1
+// 1 0 1
+// Therefore, 5 is answer
+
+// XOR (^)
+console.log(4 ^ 5);
+// 1 0 0
+// 1 0 1
+// 0 0 1
+// Therefore, 1 is answer
+
+// Shift Operator
+// Left Shift
+// 5 multiply by 2 ki power 3
+console.log(5 << 3);
+// 101.00000 = 5
+// 10100.0000 = 40
+
+// Right Shift
+// 20 divide by 2 ki power 2
+console.log(20 >> 2);
+// 10100.0000 = 20
+// 00101.0000 = 5
