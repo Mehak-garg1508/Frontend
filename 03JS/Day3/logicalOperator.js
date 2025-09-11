50f26e77093186aee13c38fa5591373bb7cbe205
Mehak Garg <missgunnu1@gmail.com>
2025-09-11 23:03:03 +0530

    Logical Operator

---
+++ 03JS/Day3/logicalOperator.js
@@ -0,0 +1,15 @@
+// Logical Opeartors
+let age = 18;
+let money = 420;
+// And Operator (&&)
+// Gives true only when both conditions are true
+console.log(age < 18 && money > 400);
+console.log(age == 18 && money > 400);
+// Or operator (||)
+// Gives false only when both statements are false
+console.log(age < 18 || money > 400);
+console.log(age < 18 || money < 400);
+
+// Not Operator (!)
+console.log(!(age > 18));
+console.log(!(money > 400));
